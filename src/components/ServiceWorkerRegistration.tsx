65252f5988b5d7c1080d1e8a7628d0543c16e53e
Meomurum Admin <admin@meomurum.com>
2026-07-23 08:57:46 +0900

    feat: use memoroom logo for notification badge

--- src/components/ServiceWorkerRegistration.tsx
+++ src/components/ServiceWorkerRegistration.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 
-const SERVICE_WORKER_URL = "/sw.js?v=20260723-3";
+const SERVICE_WORKER_URL = "/sw.js?v=20260723-4";
 
 export function ServiceWorkerRegistration() {
   useEffect(() => {
