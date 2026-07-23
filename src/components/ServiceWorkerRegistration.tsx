a7badd322d0984903bec007462520df8df7c4ae6
Meomurum Admin <admin@meomurum.com>
2026-07-23 10:01:49 +0900

    fix: maximize notification logo badge

--- src/components/ServiceWorkerRegistration.tsx
+++ src/components/ServiceWorkerRegistration.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from "react";
 
-const SERVICE_WORKER_URL = "/sw.js?v=20260723-5";
+const SERVICE_WORKER_URL = "/sw.js?v=20260723-6";
 
 export function ServiceWorkerRegistration() {
   useEffect(() => {
